a36b24cc5f6208535d7f0720a9f3eb045dda2a7b
Jean-Paul Deshaies <jdeshaies1982@gmail.com>
2019-05-05 17:16:24 -0700

    Revert "Merge branch 'master' into backend_dev"
    This reverts commit 83b201e32cc5768efb87af9195075404287efcde, reversing
    changes made to 0befbd0bfbcfd75fa0110a0a6a6acd68738b6998.

--- db/schema.sql
+++ db/schema.sql
@@ -15,8 +15,8 @@ CREATE TABLE cats
 	cat_location_id_fk INT NOT NULL,	/* used to link: 'location' with 'cat' details */
 
 	/* probably a better design */
-	--cat_location_room INT NOT NULL,
-	--cat_location_kennel INT NOT NULL,
+	cat_location_room INT NOT NULL,
+	cat_location_kennel INT NOT NULL,
 
 	PRIMARY KEY (cat_id)
 );
