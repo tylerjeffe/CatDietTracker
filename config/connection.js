65f4b4d987ae054da1bd39b212aac7a40fa5e8fa
Jean-Paul Deshaies <jdeshaies1982@gmail.com>
2019-05-11 09:29:10 -0700

    heroku connection updates

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "ocvwlym0zv3tcn68.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Earth123!",
-  database: "cat_diet_tracker_db",
+  user: "x94prr91fvar4lwd",
+  password: "r8zd4co4lrc5liub",
+  database: "j6jxgv1mso2dra1q",
   multipleStatements: true
 });
 
